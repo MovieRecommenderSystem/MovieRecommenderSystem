d272e68af495f5ebc68fba713e6e1cbde2f330c0
ravichopra0107 <shivamchopra1234567890@gmail.com>
2021-05-13 17:22:00 +0530

    [Change Model IMDB to TMDB]

--- Model/model_api.py
+++ Model/model_api.py
@@ -13,7 +13,7 @@ count_matrix=pickle.load(open('Count_Matrix.pickle', 'rb'))
 md = pd.read_csv('movies_metadata.csv',low_memory=False)
 md['genres']=md['genres'].fillna('[]').apply(literal_eval).apply(lambda x: [i['name'] for i in x] if isinstance(x,list) else [])
 md['original_language']=md['original_language'].astype('str')
-md=md[md['imdb_id'].notnull()]
+md=md[md['id'].notnull()]
 
 @app.route('/apiRecommender/simple', methods=['POST'])
 def process_request():
@@ -33,7 +33,7 @@ def process_request():
     print(genres)
     df=gen_md[gen_md['genre'].isin(genres)]
     df=df[df['original_language'].isin(languages)]
-    SR=md[md['imdb_id'].isin(df['imdb_id'])]
+    SR=md[md['id'].isin(df['id'])]
     vote_counts_SR = SR[SR['vote_count'].notnull()]['vote_count'].astype('int')
     vote_averages_SR = SR[SR['vote_average'].notnull()]['vote_average'].astype('int')
     C_SR = vote_averages_SR.mean()
@@ -43,7 +43,7 @@ def process_request():
     qualified_SR['vote_average'] = qualified_SR['vote_average'].astype('int')
     qualified_SR['wr'] = ((qualified_SR['vote_count']*qualified_SR['vote_average'])+(m_SR*C_SR))/(qualified_SR['vote_count']+m_SR)
     qualified_SR=qualified_SR.sort_values('wr',ascending=False)
-    answer = qualified_SR[['imdb_id']].head(10).values.tolist()
+    answer = qualified_SR[['id']].head(10).values.tolist()
     answer = list(np.concatenate(answer).flat)
     f = time.time()
     print(f-i)
@@ -54,18 +54,17 @@ def process_request2():
 
     #Parse received JSON request
     user_input = request.get_json()
-    imdb = user_input['IMDB']
-
+    tmdb = user_input['TMDB']
     try:
-        IDX=smd.index[smd['imdb_id']==imdb][0]
+        IDX=smd.index[smd['id']==tmdb][0]
     except:
         return "NA"
     cos_sim=cosine_similarity(count_matrix[IDX],count_matrix).flatten()
     sim_scores=list(enumerate(cos_sim))
     sim_scores=sorted(sim_scores, key=lambda x:x[1], reverse=True)
     sim_scores=sim_scores[1:11]
-    response = {'result': [smd.iloc[t_vect[0]].loc["imdb_id"] for t_vect in sim_scores]}
-    print(response['result'])
+    response = {'result': [smd.iloc[t_vect[0]].loc["id"] for t_vect in sim_scores]}
+    response = {"response": list(map(str,response["result"]))}
     return jsonify(response)
 
 if __name__ == '__main__':
